88f844743d5b157665ad5f7fdf8740dd3ef9f7dc
mishrarpita321 <45449505+mishrarpita321@users.noreply.github.com>
2024-04-19 00:57:18 +0200

    changes

--- src/components/Combined/Combined.jsx
+++ src/components/Combined/Combined.jsx
@@ -1,16 +1,17 @@
 import IFrame from "../IFrame/IFrame"
+import IFrameVideo from "../IFrame/IFrameVideo";
 
 const Combined = () => {
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-md-6">
-                    <h1 style={{color: 'blueviolet', fontFamily: 'cursive'}}>Upload Video</h1>
+                    <h1 style={{color: 'blueviolet', fontFamily: 'cursive'}}>Upload Image</h1>
                     <IFrame />
                 </div>
                 <div className="col-md-6">
-                    <h1 style={{color: 'blueviolet', fontFamily: 'cursive'}}>Upload Image</h1>
-                    <IFrame />
+                    <h1 style={{color: 'blueviolet', fontFamily: 'cursive'}}>Upload Video</h1>
+                    <IFrameVideo />
                 </div>
             </div>
         </div>
--- src/components/IFrame/IFrameVideo.jsx
+++ src/components/IFrame/IFrameVideo.jsx
@@ -1,15 +1,13 @@
 import UploadBtn from "../UploadButton/UploadBtn";
 import React, { useState } from 'react';
 
-const IFrame = () => {
+const IFrameVideo = () => {
     const [fileSrc, setFileSrc] = useState(null);
     return (
         <>
             <iframe
                 title="file-preview"
-                // src={fileSrc ? fileSrc : 'https://www.youtube.com/embed/dQw4w9WgXcQ'}
-                // src={fileSrc}
-                srcDoc={fileSrc ? `<style>body, html {width: 100%; height: 100%; margin: 0; padding: 0; overflow: hidden;}</style><img src="${fileSrc}" style="width: 100%; height: 100%; object-fit: cover;" />` : ''}
+                src={fileSrc}
                 allowFullScreen
                 style={{
                     width: '100%',
@@ -23,4 +21,4 @@ const IFrame = () => {
     );
 };
 
-export default IFrame;
+export default IFrameVideo;
