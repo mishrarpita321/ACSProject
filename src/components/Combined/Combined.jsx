db3cce44e4f7a3863d648fa000ee98cfd7ecfd96
mishrarpita321 <45449505+mishrarpita321@users.noreply.github.com>
2024-04-20 15:40:46 +0200

    changes

--- src/components/Combined/Combined.jsx
+++ src/components/Combined/Combined.jsx
@@ -3,15 +3,27 @@ import IFrame from "../IFrame/IFrame"
 
 const Combined = () => {
     return (
-        <div className="container-fluid">
+        <div className="container">
             <div className="row">
                 <div className="col-md-6">
-                    <h1 style={{color: 'blueviolet', fontFamily: 'cursive'}}>Upload Video</h1>
+                    <h1 style={{ color: 'blueviolet', fontFamily: 'cursive' }}>Upload Video</h1>
                     <IFrame />
                 </div>
                 <div className="col-md-6">
-                    <h1 style={{color: 'blueviolet', fontFamily: 'cursive'}}>Upload Image</h1>
+                    <h1 style={{ color: 'blueviolet', fontFamily: 'cursive' }}>Upload Image</h1>
                     <IFrame />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="col-md-8">
+                        <EmojiPicker />
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="col-md-8">
+                        <EmojiPicker />
+                    </div>
                 </div>
             </div>
         </div>
