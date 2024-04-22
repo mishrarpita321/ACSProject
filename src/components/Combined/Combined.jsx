e9577fc258db873e364a14c52f38d448a5ea8bc3
Roshan Devkota <devkkali@gmail.com>
2024-04-23 01:00:14 +0200

    comparison table added (only ui)

--- src/components/Combined/Combined.jsx
+++ src/components/Combined/Combined.jsx
@@ -4,13 +4,15 @@ import IFrameVideo from "../IFrame/IFrameVideo";
 import People from "../People/People";
 import styles from './Combined.module.css'
 import Header from "../Header/Header";
+import TableLoader from "../table_placeholder_loader/TableLoader";
+import ComparisonTable from "../Comparison/ComparisonTable";
 
 const Combined = () => {
     return (
         <>
             <Header />
             <div className="container">
-                <div style={{position:"relative"}} className="row">
+                <div style={{ position: "relative" }} className="row">
                     <div className={`col-12 col-md-6 ${styles.iFrameContainer}`}>
                         <h1 style={{ color: '#8e62a0', fontFamily: 'sans-serif' }}>Upload Video</h1>
                         <IFrameVideo />
@@ -26,6 +28,17 @@ const Combined = () => {
                         <People />
                     </div>
                 </div>
+
+
+
+                {/* implementation is remaning */}
+
+                {/* 
+                <div style={{marginTop:`50px`}} className="row">
+                    <TableLoader />
+                    <ComparisonTable />
+                </div>
+                */}
             </div>
         </>
     );
