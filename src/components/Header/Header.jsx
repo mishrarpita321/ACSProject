9a166e9906b54e283e6b9b130d84caa22553418f
Roshan Devkota <devkkali@gmail.com>
2024-04-27 23:22:08 +0200

    Heading Title dynamic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import styles from './Header.module.css';
 
-const Header = ({ setFileSrc }) => {
+const Header = ({ title = "", setFileSrc, showCompareBtn = false }) => {
     return (
         <>
             <div className={styles.appBar}>
-                <h1 className={styles.appBarTitle}>Vision API vs. Video Intelligence API</h1>
+                {/* <h1 className={styles.appBarTitle}>Vision API vs. Video Intelligence API</h1> */}
+                <h1 className={styles.appBarTitle}>{title}</h1>
             </div>
-            <div className={styles.floating_btn}>
-                &#x1F50D; Compare
-            </div>
+            {showCompareBtn && (
+                <div className={styles.floating_btn}>
+                    &#x1F50D; Compare
+                </div>
+            )}
         </>
     );
 };
