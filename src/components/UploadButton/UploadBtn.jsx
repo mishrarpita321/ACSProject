8de749f7729915750533e7e5d5a2ce6cd5b91d0e
Roshan Devkota <156541904+roshandevkota@users.noreply.github.com>
2024-05-04 21:19:08 +0200

    page 2 completed
    but have one issue, on timestap hover, on fast movement its misplacing the tooltip,

--- src/components/UploadButton/UploadBtn.jsx
+++ src/components/UploadButton/UploadBtn.jsx
@@ -1,17 +1,33 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from './UploadBtn.module.css';
 import axios from 'axios';
 import transformApiResponse from '../Utils/Transformdata';
 import Loader from '../Loader/Loader';
 import { BsArrowClockwise } from "react-icons/bs"; // Importing the refresh icon from react-icons
+import transformCombinedApiResponse from '../Utils/transformCombinedApiResponse';
 
-const UploadBtn = ({ widthInPercentage = "60%", setFileSrc, setShowEmoji, fileType, setVisionData, setShowLoader, showLoader, setVideoData, setShowFaces }) => {
+const UploadBtn = ({ widthInPercentage = "60%", setCombinedData, setFileSrc, setShowEmoji, fileType, setVisionData, setShowLoader, showLoader, setVideoData, setShowFaces }) => {
     const [file, setFile] = useState(null);
     const [fileName, setFileName] = useState('');
     const [uploadError, setUploadError] = useState(false);
     const fileInputRef = useRef(null);
+    const [apiType, setApiType] = useState('imge');
 
     const [justifyContent, setJustifyContent] = useState('space-between');
+
+
+
+
+    useEffect(() => {   // This useEffect will be called whenever the fileType changes
+        if (fileType === 'video') {
+            setApiType('video');
+        } else if (fileType === 'image') {
+            setApiType('image');
+        } else {
+            setApiType('combined');
+        }
+    }, [fileType]);
+
 
     const handleFileChange = async (e) => {
         const newFile = e.target.files[0];
@@ -27,25 +43,56 @@ const UploadBtn = ({ widthInPercentage = "60%", setFileSrc, setShowEmoji, fileTy
         setUploadError(false);
         setShowLoader(true);
         const formData = new FormData();
-        formData.append(fileType === 'video' ? 'video' : 'image', fileToUpload);
         const url = URL.createObjectURL(fileToUpload);
         setFileSrc(url);
 
+        let apiEndpoint = '';
+
+
+        if (fileType === 'image') {
+            apiEndpoint = 'https://acs-example1.ey.r.appspot.com/api/vision'; // Default API endpoint
+            formData.append('image', fileToUpload);
+        } else if (fileType === 'video') {
+            apiEndpoint = 'https://acs-example1.ey.r.appspot.com/api/videointelligence';
+            formData.append('video', fileToUpload);
+        } else if (apiType === 'combined') {
+            formData.append('video', fileToUpload);
+            apiEndpoint = 'https://acs-example1.ey.r.appspot.com/api/combined'; // API endpoint for combined data
+        }
+
+
+
         try {
             const response = await axios.post(
-                fileType === 'video' ? 'https://acs-example1.ey.r.appspot.com/api/videointelligence' : 'https://acs-example1.ey.r.appspot.com/api/vision',
+                apiEndpoint,
                 formData,
                 { headers: { 'Content-Type': 'multipart/form-data' } }
             );
 
             if (response.status === 200) {
-                const data = fileType === 'video' ? response.data : transformApiResponse(response.data);
+                console.log('Upload Success:', response.data);
+                // if (fileType === 'video') {
+                // } else if (fileType === 'image') {
+                // }
+                // else {
+                // }
+                let data = response.data;
+
+
+                // data = fileType === 'video' ? response.data : transformApiResponse(response.data);
+
                 if (fileType === 'video') {
+                    data= response.data;
                     setVideoData(data);
                     setShowFaces(true);
-                } else {
+                } else if (fileType === 'image') {
+                    data = transformApiResponse(response.data);
                     setVisionData(data);
                     setShowEmoji(true);
+                }else {
+                    data = transformCombinedApiResponse(response.data);
+                    setCombinedData(data);
+                    setShowFaces(true);
                 }
             }
         } catch (error) {
@@ -82,7 +129,7 @@ const UploadBtn = ({ widthInPercentage = "60%", setFileSrc, setShowEmoji, fileTy
                         id="file-upload"
                         ref={fileInputRef}
                         onChange={handleFileChange}
-                        accept={fileType === 'video' ? 'video/*' : 'image/png, image/jpeg'}
+                        accept={fileType === 'video' || fileType === 'combined' ? 'video/*' : 'image/png, image/jpeg'}
                         style={{ display: 'none' }}
                     />
                     <div className={styles.flexContainer}>
